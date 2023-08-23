ea7d5fb6bbbe920b82f8e8f1e5c49b84aa56b4f8
Mirko Bozzetto <mirkouch@gmail.com>
2023-08-23 15:31:06 +0200

    lang + nex txt

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
+// * Padding interactif
 const computePadding = (open) =>
   `padding-top: ${open ? "10rem" : "4rem"} + 5vh`;
-
 window.computePadding = computePadding;
+//______________________________________________________________________________
+
+// * Gestion du saut de lignes
