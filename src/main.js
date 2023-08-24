e39efee80a708a5a7e1aeb062ab73ac492b25ec1
Mirko Bozzetto <mirkouch@gmail.com>
2023-08-24 13:46:59 +0200

    interaction nav & up btm ok

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@
 const computePadding = (open) =>
   `padding-top: ${open ? "10rem" : "4rem"} + 5vh`;
 window.computePadding = computePadding;
-//______________________________________________________________________________
 
-// * Gestion du saut de lignes
+// * smooth nav scroll
+
+document.querySelectorAll('a[href^="#"]').forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    let target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
